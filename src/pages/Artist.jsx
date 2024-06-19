4b3000a153d49bd7f9e2f11f473ef72535607c6b
Constant <110848773+joaopconstant@users.noreply.github.com>
2024-06-19 02:30:12 -0300

    Modal Fix

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -79,7 +79,7 @@ export default function Artist() {
 
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white/5 p-4 rounded-md">
+          <div className="bg-zinc-900 p-4 rounded-md">
             <h2 className="text-2xl mb-4">Adicionar Música</h2>
             <input
               type="text"
