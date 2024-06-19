30d15a262b408fd20bcc4ceb16a6d1988a651b87
Constant <110848773+joaopconstant@users.noreply.github.com>
2024-06-19 03:46:29 -0300

    Quick Fix

--- src/pages/NewArtist.jsx
+++ src/pages/NewArtist.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Footer from '../components/Footer';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 export default function NewArtist() {
   const [nomeBanda, setNomeBanda] = useState('');
@@ -10,6 +11,8 @@ export default function NewArtist() {
   const [nomeMusica, setNomeMusica] = useState('');
   const [imagemMusica, setImagemMusica] = useState('');
   const [duracao, setDuracao] = useState('');
+  const [bandaId, setBandaId] = useState(null); // Estado para armazenar o ID da banda criada
+  const history = useHistory();
 
   const handleCadastroBanda = async () => {
     try {
@@ -26,7 +29,9 @@ export default function NewArtist() {
 
       const response = await axios.post('https://ibmec-cloud-backend-appservice.azurewebsites.net/banda', bandaData);
       console.log('Banda cadastrada:', response.data);
-      // Aqui você pode redirecionar o usuário para a página da banda recém-criada, se desejar
+      setBandaId(response.data.id); // Armazena o ID da banda recém-criada
+      // Redireciona para a página da banda recém-criada
+      history.push(`/banda/${response.data.id}`);
     } catch (error) {
       console.error('Erro ao cadastrar banda:', error);
     }
@@ -37,10 +42,10 @@ export default function NewArtist() {
       <div className="flex flex-1">
         <Sidebar />
         <main className="flex-1 p-6">
-          <h1 className="text-3xl font-semibold mb-6">Cadastro de Banda</h1>
+          <h1 className="text-3xl font-semibold mb-6">Cadastro de Artista</h1>
           <div className="grid grid-cols-1 gap-4 px-20">
             <div>
-              <label htmlFor="nomeBanda" className="block text-sm font-semibold">Nome da Banda</label>
+              <label htmlFor="nomeBanda" className="block text-sm font-semibold">Nome do Artista</label>
               <input type="text" id="nomeBanda" className="p-2 bg-zinc-900 border rounded-md w-full" value={nomeBanda} onChange={(e) => setNomeBanda(e.target.value)} />
             </div>
             <div>
@@ -48,7 +53,7 @@ export default function NewArtist() {
               <textarea id="descricao" className="p-2 bg-zinc-900 border rounded-md w-full" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
             </div>
             <div>
-              <label htmlFor="imagemBanda" className="block text-sm font-semibold">URL da Imagem da Banda</label>
+              <label htmlFor="imagemBanda" className="block text-sm font-semibold">URL da Imagem do Artista</label>
               <input type="text" id="imagemBanda" className="p-2 bg-zinc-900 border rounded-md w-full" value={imagemBanda} onChange={(e) => setImagemBanda(e.target.value)} />
             </div>
             <div>
